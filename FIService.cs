0a03edeb5cb289fac4c5edd9a3fa87c9981bf507
osalami <osalami@afreximbank.com>
2023-09-01 12:58:02 +0300

    first commit

---
+++ FIService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using Microsoft.Extensions.Configuration;
+
+namespace TestFIService
+{
+	public class FIService
+	{
+        private IConfiguration _configuration;
+
+        public FIService(IConfiguration configuration)
+		{
+            _configuration = configuration;
+		}
+
+        public static bool TrustAllCertificateCallback(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors errors)
+        {
+
+            return true;
+
+        }
+
+        [Obsolete]
+        public string InvokeService(string XMLData)
+        {
+            string serviceResponse = string.Empty;
+
+            try
+            {
+                //Trust All Certificates
+                ServicePointManager.ServerCertificateValidationCallback = TrustAllCertificateCallback;
+
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                       | SecurityProtocolType.Tls11
+                       | SecurityProtocolType.Tls12;
+
+                //Calling CreateSOAPWebRequest method  
+                HttpWebRequest request = CreateSOAPWebRequest();
+                XmlDocument SOAPReqBody = new XmlDocument();
+                //SOAP Body Request  
+                SOAPReqBody.LoadXml(XMLData);
+                using (Stream stream = request.GetRequestStream())
+                {
+                    SOAPReqBody.Save(stream);
+                }
+                //Geting response from request  
+                using (WebResponse Serviceres = request.GetResponse())
+                {
+                    using (StreamReader rd = new StreamReader(Serviceres.GetResponseStream()))
+                    {
+
+                        var ServiceResult = rd.ReadToEnd();
+                        serviceResponse = ServiceResult.Trim();
+                        Console.WriteLine("Response / Invoke Webservice XML ==> " + serviceResponse + "     --------@ " + DateTime.Now);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error in SOAP Webservice InvokeService " + ex.ToString() + " @ " + DateTime.Now);
+            }
+
+
+
+            return serviceResponse.Trim();
+        }
+
+        [Obsolete]
+        private HttpWebRequest CreateSOAPWebRequest()
+        {
+            //Make Web Request  
+            HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(_configuration["FinacleSoapURL"]);
+            //SOAP Action  
+            Req.Headers.Add("SOAPAction:" + _configuration["FinacleSoapWSDL"]);
+            //Content_type  
+            Req.ContentType = "text/xml;charset=\"utf-8\"";
+            Req.Accept = "text/xml";
+            //HTTP method  
+            Req.Method = "POST";
+            //return HttpWebRequest  
+            return Req;
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace TestFIService;
+
+class Program
+{
+    [Obsolete]
+    static void Main(string[] args)
+    {
+       
+
+        try
+        {
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var appSettingsName = string.IsNullOrEmpty(environment) ? "appsettings.json" : $"appsettings.{environment}.json";
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(appSettingsName);
+
+            var configuration = builder.Build();
+            Console.WriteLine("Testing FI Service");
+            string filePath = $"{assemblyPath}{configuration["XmlFileName"]}";
+            string AcctInqXml = File.ReadAllText(filePath);
+            Console.WriteLine($"XML File Read \n {AcctInqXml}");
+
+            FIService fIService = new FIService(configuration);
+
+            var response = fIService.InvokeService(AcctInqXml);
+
+            Console.WriteLine($"XML Response from FI \n {response}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error " + ex.ToString());
+        }
+
+        Console.Write("Press Enter to Exit .....");
+        Console.ReadLine();
+    }
+}
+
